ab9742617d3439b88dbb167676b9ec717243c08a
tdemierr <timothee.demierre@epfl.ch>
2016-08-29 19:40:18 +0200

    Clean

--- test.js
+++
@@ -1,7 +0,0 @@
-var io = require('socket.io').listen(80);
-
-io.sockets.on('connection', function (socket) {
-    socket.on("getSomeData", function(name,fn) {
-        fn({data: "some random data"});
-    });
-});
