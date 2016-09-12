cdb32691fe9a93dca2b8f51de665a836ff56a903
tdemierr <timothee.demierre@epfl.ch>
2016-09-12 19:22:57 +0200

    Power Amp 2

--- IRManager.py
+++ IRManager.py
@@ -8,7 +8,7 @@ class IRManager:
 
     def sendPower(self):
         os.system(self.irsendconstant + self.ampCode + " " + "Key_Power")
-        print "Power"
+        print self.irsendconstant + self.ampCode + " " + "Key_Power"
     def __init__(self, AmpCodes, JukeboxCode):
         self.ampCode=AmpCodes
         self.jukeboxCode=JukeboxCode
